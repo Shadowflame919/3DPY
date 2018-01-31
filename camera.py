4e9c13a12c887b53d13104be4202d55e2514d357
Matty <shadowflame919@gmail.com>
2018-01-31 20:03:51 +1100

    Main class, mouse & position movement
    Moved everything in init to a main class.
    Also fixed mouse movement/locking which can be toggled with ESC.
    Player can move with WASD, SPACE & SHIFT and movement corresponds with
    the cameras direction.

--- camera.py
+++ camera.py
@@ -4,12 +4,17 @@ import math, vector, pygame
 class Camera():
     def __init__(self, screen, res):
         self.pos = vector.Vector3(0,0,0)
-        self.dir = vector.Vector3(0,0,0)
+
+        # Not actually a vector, x represents looking right/left, y is up/down, z is just rotating view
+        # Default (0,0,0) is looking in the positive x direction (left handed coordinate system)
+        self.dir = vector.Vector3(0,0,0)    
 
         self.screen = screen    # Screen to render camera on
         self.res = res      # Resolution of camera
 
         self.fov = math.pi/3
+
+        self.speed = 10
 
     def render(self):   # Renders the camera's view on the screen
 
@@ -17,4 +22,35 @@ class Camera():
         pygame.draw.line(self.screen, [0,0,0], [self.res[0]/2-15,self.res[1]/2], [self.res[0]/2+15,self.res[1]/2], 3)
         pygame.draw.line(self.screen, [0,0,0], [self.res[0]/2,self.res[1]/2-15], [self.res[0]/2,self.res[1]/2+15], 3)
 
+    def changePos(self, dt, pressedKeys):
+        if pressedKeys[pygame.K_w]:
+            self.pos.x += dt * self.speed * math.cos(self.dir.x)
+            self.pos.z += dt * self.speed * math.sin(self.dir.x)
+        if pressedKeys[pygame.K_s]:
+            self.pos.x -= dt * self.speed * math.cos(self.dir.x)
+            self.pos.z -= dt * self.speed * math.sin(self.dir.x)
+        if pressedKeys[pygame.K_a]:
+            self.pos.x -= dt * self.speed * math.sin(self.dir.x)
+            self.pos.z += dt * self.speed * math.cos(self.dir.x)
+        if pressedKeys[pygame.K_d]:
+            self.pos.x += dt * self.speed * math.sin(self.dir.x)
+            self.pos.z -= dt * self.speed * math.cos(self.dir.x)
+        if pressedKeys[pygame.K_SPACE]:
+            self.pos.y += dt * self.speed
+        if pressedKeys[pygame.K_LSHIFT]:
+            self.pos.y -= dt * self.speed
 
+    def changeDir(self, mouseMove):
+        if mouseMove[0] != 0:
+            self.dir.x -= mouseMove[0] / 1000
+            if (self.dir.x < -math.pi): 
+                self.dir.x += 2*math.pi
+            elif (self.dir.x > math.pi): 
+                self.dir.x -= 2*math.pi
+
+        if mouseMove[1] != 0:
+            self.dir.y -= mouseMove[1] / 1000
+            if (self.dir.y > math.pi/2): 
+                self.dir.y = math.pi/2
+            elif (self.dir.y < -math.pi/2): 
+                self.dir.y = -math.pi/2
--- init.py
+++ init.py
@@ -5,48 +5,62 @@ pygame.init()
 
 import vector, camera
 
-res = [960, 540]
-screen = pygame.display.set_mode(res)
-clock = pygame.time.Clock()
 
-cam = camera.Camera(screen, res)
+class Main():
+	def __init__(self):
+		self.res = (960, 540)
+		self.screen = pygame.display.set_mode(self.res)
+		self.clock = pygame.time.Clock()
 
-font = pygame.font.SysFont(None, 24)
-#textrect = text.get_rect()
-#textrect.centerx = screen.get_rect().centerx
-#textrect.centery = screen.get_rect().centery
+		self.cam = camera.Camera(self.screen, self.res)
 
-mouseLock = False
+		self.font_log = pygame.font.SysFont(None, 24)
+		#textrect = text.get_rect()
+		#textrect.centerx = screen.get_rect().centerx
+		#textrect.centery = screen.get_rect().centery
+
+		self.mouseLock = False
+
+	def start(self):	# Starts the main update loop
+		while True:
+			dt = self.clock.tick()/1000
+			if self.mouseLock:
+				pygame.mouse.set_pos = (self.res[0]/2, self.res[1]/2)
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT: 
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE:
+						self.mouseLock = not self.mouseLock
+						pygame.mouse.set_visible(not self.mouseLock)
+						pygame.event.set_grab(self.mouseLock)
+				if event.type == pygame.MOUSEMOTION:
+					if self.mouseLock:
+						self.cam.changeDir(event.rel)
+			
+			# Moves camera
+			if self.mouseLock:
+				self.cam.changePos(dt, pygame.key.get_pressed())
+
+			self.screen.fill([150,250,250])
+			self.cam.render()
+
+
+			text = self.font_log.render(str(round(self.clock.get_fps(), 2)), True, (0, 0, 0))
+			self.screen.blit(text, [10,10])
+
+			text = self.font_log.render(str(self.cam.pos), True, (0, 0, 0))
+			self.screen.blit(text, [10,30])
+
+			text = self.font_log.render(str(self.cam.dir), True, (0, 0, 0))
+			self.screen.blit(text, [10,50])
+
+			pygame.display.flip()
+
+
+main = Main()
+main.start()
 
 
 
-while True:
-	dt = clock.tick()/1000
-	if mouseLock:
-		pygame.mouse.set_pos = (res[0]/2, res[1]/2)
-
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: 
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				mouseLock = not mouseLock
-				pygame.mouse.set_visible(not mouseLock)
-	
-	if pygame.key.get_pressed()[pygame.K_w]:
-		cam.pos.x += 1 * dt
-
-	screen.fill([150,250,250])
-	cam.render()
-
-
-	text = font.render(str(round(clock.get_fps(), 2)), True, (0, 0, 0))
-	screen.blit(text, [10,10])
-
-	text = font.render(str(cam.pos), True, (0, 0, 0))
-	screen.blit(text, [10,30])
-
-	print(mouseLock)
-
-	pygame.display.flip()
